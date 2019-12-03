804ae355e85ca0c91327b0a4fde8722c0e3a58b9
Andrew Randan <andrewrandan@gmail.com>
2019-12-03 16:58:57 +0200

    added logging

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 
 const subscribersRouter = require('./routes/subscribers');
+const logging = require('./middlewares/logging');
 
 mongoose.connect(
     process.env.DATABASE_URL,
@@ -16,6 +17,7 @@ db.on('error', error => console.log(error));
 db.once('open', () => console.log('connected to database'));
 
 app.use(express.json());
+app.use(logging);
 
 app.use('/subscribers', subscribersRouter);
 
